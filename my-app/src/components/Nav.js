0dd3ab99b044e3cdf8d9f65d3cce4646c63ef7c8
Sungjin Lim <85204388+Sung-jin-Lim@users.noreply.github.com>
2022-04-18 07:11:36 +0700

    base files

---
+++ my-app/src/components/Nav.js
@@ -0,0 +1,18 @@
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav-item">
+        <p>Where in the world?</p>
+      </div>
+
+      <div className="nav-item">
+        <div className="theme">Dark Mode</div>
+      </div>
+
+    </div>
+  )
+
+}
+
+export default Nav;
